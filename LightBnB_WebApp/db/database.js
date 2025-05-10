498d43d00761df8cebc8d4c05504df804b7a7f8b
Brendan Brown <brendan94brown@gmail.com>
2025-05-10 15:54:54 -0700

    Change getAllReservations to return data from the database

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -52,7 +52,7 @@ const getUserWithId = function (id) {
       return result.rows[0];
     })
 };
-
+ 
 /**
  * Add a new user to the database.
  * @param {{name: string, password: string, email: string}} user
@@ -78,7 +78,20 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`
+      SELECT reservations.*, properties.*, AVG(property_reviews.rating) as average_rating
+      FROM reservations
+      JOIN properties ON reservations.property_id = properties.id
+      JOIN property_reviews ON properties.id = property_reviews.property_id
+      WHERE reservations.guest_id = $1
+      GROUP BY reservations.id, properties.id
+      ORDER BY reservations.start_date
+      LIMIT $2;
+    `, [guest_id, limit])
+    .then((result) => {
+      return result.rows;
+    });
 };
 
 /// Properties
