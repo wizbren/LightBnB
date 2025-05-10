70f0ea6c4e87f90b23804f6764e1e8d772e9f6b0
Brendan Brown <brendan94brown@gmail.com>
2025-05-10 15:37:04 -0700

    Change addUser to use database

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -59,10 +59,15 @@ const getUserWithId = function (id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function (user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  return pool
+   .query(`
+    INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3)
+    RETURNING *;
+    `, [user.name, user.email, user.password])
+    .then ((result) => {
+      return result.rows[0];
+    });
 };
 
 /// Reservations
